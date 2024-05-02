fda1d4d601a7e0b3b6610bf7189dd9be19a0dfb9
zafifi100 <114939084+zafifi100@users.noreply.github.com>
2024-05-01 21:29:14 -0500

    cache control added more logic

--- CacheControl.v
+++ CacheControl.v
@@ -1,23 +1,30 @@
 `default_nettype none
 
 // TODO LIST
-/* Data inputs to caches
- * word enable logic
- * what bits are checked from tagout for miss detected logic
- * memory contention
- * LRU 
- * add decoders and one hots for enables
+/* 6 - 64 decoder: block enable
+ * 3 - 8 decoder: word enable
+ * LRU logic
+ * valid bit logic
+ * miss address logic
+ */
+
+/*
+ *TAG: 5-0: tag, bit 6: valid bit, bit 7: LRU bit
  */
 module CacheControl(
   input wire clk, rst,
-  input wire [15:0] miss_address, curr_pc, // input to cache fsm
+  input wire [15:0] curr_pc, // input to cache fsm
   input wire [15:0] dcache_data, // input data to dcache
   input wire [15:0] newdata, // new data to write to main mem and cache
   output wire [15:0] icache_out, dcache_out, // output data from each cache
   output wire fsm_busy
 );
   // cache data to be written
-  wire [5:0] idata_in, ddata_in;
+  wire [15:0] idata_in, ddata_in;
+  wire [5:0] imetadata0_in, dmetadata0_in, imetadata1_in, dmetadata1_in;
+
+  // LRU and valid bit logic for caches
+  wire iLRU0_bit, ivalid0_bit, iLRU1_bit, ivalid1_bit, dLRU0_bit, dLRU1_bit, dvalid0_bit, dvalid1_bit;
 
   // cache enables
   wire icache_en, dcache_en;
@@ -35,25 +42,26 @@ module CacheControl(
   wire [7:0] TagOut0_dcache, TagOut1_dcache;
   wire [15:0] DataOut0_dcache, DataOut1_dcache;
 
- 
   //memory outputs
   wire [15:0] MainMemOut;
 
+  // assign miss address based on dcache or icache
+  wire [15:0] miss_address;
 
   // instruction cache (TODO: Complete port hookups & check write enable doesnt need more)
   // Way 0 icache
-  MetaDataArray imetarray0(.clk(clk), .rst(rst), .DataIn(memory_address[15:10]), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut0_icache));
+  MetaDataArray imetarray0(.clk(clk), .rst(rst), .DataIn(imetadata0_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut0_icache));
   DataArray idatarray0(.clk(clk), .rst(rst), .DataIn(idata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut0_icache));
   // Way 1 icache
-  MetaDataArray imetarray1(.clk(clk), .rst(rst), .DataIn(), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut1_icache));
+  MetaDataArray imetarray1(.clk(clk), .rst(rst), .DataIn(imetadata1_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut1_icache));
   DataArray idatarray1(.clk(clk), .rst(rst), .DataIn(idata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut1_icache));
 
   // data cache (TODO: Complete port hookups & check write enable doesnt need more)
   // Way 0 dcache
-  MetaDataArray dmetarray0(.clk(clk), .rst(rst), .DataIn(), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut0_dcache));
+  MetaDataArray dmetarray0(.clk(clk), .rst(rst), .DataIn(dmetadata0_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut0_dcache));
   DataArray ddatarray0(.clk(clk), .rst(rst), .DataIn(ddata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut0_dcache));
   // Way 1 dcache
-  MetaDataArray dmetarray1(.clk(clk), .rst(rst), .DataIn(), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut1_dcache));
+  MetaDataArray dmetarray1(.clk(clk), .rst(rst), .DataIn(dmetadata1_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut1_dcache));
   DataArray ddatarray1(.clk(clk), .rst(rst), .DataIn(ddata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut1_dcache));
 
 
@@ -64,18 +72,22 @@ module CacheControl(
 
   
   // Determine cache miss (TODO: WHAT BITS TO CHECK FOR TAG)
-  assign icache_miss_detected = (curr_pc[15:10] === TagOut0_icache[5:0]) || (curr_pc[15:10] === TagOut1_icache[5:0]) ||
-                                (curr_pc[15:10] === DataOut0_icache[5:0]) || (curr_pc[15:10] === DataOut1_icache[5:0]);
+  assign icache_miss_detected = ((curr_pc[15:10] === TagOut0_icache[5:0]) & TagOut0_icache[7]) || ((curr_pc[15:10] === TagOut1_icache[5:0]) & TagOut1_icache[7]) ||
+                                ((curr_pc[15:10] === DataOut0_icache[5:0]) & TagOut0_icache[7])|| ((curr_pc[15:10] === DataOut1_icache[5:0]) & TagOut1_icache[7]);
 
-  assign dcache_miss_detected = (curr_pc[15:10] === TagOut0_dcache[5:0]) || (curr_pc[15:10] === TagOut1_dcache[5:0]) ||
-                                (curr_pc[15:10] === DataOut0_dcache[5:0]) || (curr_pc[15:10] === DataOut1_dcache[5:0]);
+  assign dcache_miss_detected = ((curr_pc[15:10] === TagOut0_dcache[5:0]) & TagOut0_dcache[7]) || ((curr_pc[15:10] === TagOut1_dcache[5:0]) & TagOut1_dcache[7]) ||
+                                (curr_pc[15:10] === DataOut0_dcache[5:0]) & TagOut0_dcache[7]|| ((curr_pc[15:10] === DataOut1_dcache[5:0])& TagOut1_dcache[7]);
 
   
   // input data to be written to dcache
-  assign ddata_in = dcache_data;
+  assign ddata_in = MainMemOut;
+  assign dmetadata0_in = {dLRU0_bit, dvalid0_bit, memory_address[15:10]};
+  assign dmetadata1_in = {dLRU1_bit, dvalid1_bit, memory_address[15:10]};
 
   // input data to be written to icache
   assign idata_in = MainMemOut;
+  assign imetadata0_in = {iLRU0_bit, ivalid0_bit, memory_address[15:10]};
+  assign imetadata1_in = {iLRU1_bit, ivalid1_bit, memory_address[15:10]};
 
  
   // Memory module (TODO: WHAT IS DATA INPUT)
@@ -83,4 +95,5 @@ module CacheControl(
                    .wr(write_data_array), .clk(clk), .rst(rst), .data_valid(memory_data_valid));
 
 endmodule
+
 
