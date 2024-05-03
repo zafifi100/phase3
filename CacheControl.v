8082fbaf6c4b5e9e35989bfe5a0a5472b49549e3
Rago200 <rsenthilkuma@wisc.edu>
2024-05-02 20:25:12 -0500

    Finished the cache.

--- CacheControl.v
+++ CacheControl.v
@@ -19,6 +19,7 @@ module CacheControl(
   output wire [15:0] icache_out, dcache_out, // output data from each cache
   output wire fsm_busy
 );
+
   // cache data to be written
   wire [15:0] idata_in, ddata_in;
   wire [5:0] imetadata0_in, dmetadata0_in, imetadata1_in, dmetadata1_in;
@@ -48,21 +49,46 @@ module CacheControl(
   // assign miss address based on dcache or icache
   wire [15:0] miss_address;
 
+  // Decode logic
+  wire [63:0] iblockenable, dblockenable; // blockenable one hot encoded
+  wire [7:0] iwordenableinstur, dwordenableinstur; // word enable one hot encoded
+  wire [6:0] iwordenablemiss, dwordenablemiss; //word enable one hot encoded (miss)
+  wire [6:0] iwordenable, dwordenable; //Actuall wordenable to be used based on a miss or hit
+
+  //Proccesor input
+  decoder3_8 iworddecoder(.decode_in(curr_pc[3:1]), .decode_out(iwordenableinstur));
+  decoder3_8 dworddecoder(.decode_in(dcache_data[3:1]), .decode_out(dwordenableinstur));
+
+  //The block should be the same on a hit and miss since the adress is the same as the missadress
+  //Only word offsets change
+  decoder6_64 iblockdecoder(.decode_in(curr_pc[9:4]), .decode_out(iblockenable));
+  decoder6_64 dblockdecoder(.decode_in(dcache_data[9:4]), .decode_out(dblockenable));
+
+  //Memory input on a miss
+  decoder3_8 iworddecodermiss(.decode_in(miss_address[3:1]), .decode_out(iwordenablemiss));
+  decoder3_8 dworddecodermiss(.decode_in(miss_address[3:1]), .decode_out(dwordenablemiss));
+
+  //Detmining which word enable to use
+  assign iwordenable = (icache_miss_detected) ? iwordenablemiss : iwordenableinstur;
+  assign dwordenable = (dcache_miss_detected) ? dwordenablemiss : iwordenablemiss;
+
+  //TODO: Need logic to determine what way to write to when we have a hit on a save word or miss and we need  to write the data one of the data blocks.
+
   // instruction cache (TODO: Complete port hookups & check write enable doesnt need more)
   // Way 0 icache
-  MetaDataArray imetarray0(.clk(clk), .rst(rst), .DataIn(imetadata0_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut0_icache));
-  DataArray idatarray0(.clk(clk), .rst(rst), .DataIn(idata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut0_icache));
+  MetaDataArray imetarray0(.clk(clk), .rst(rst), .DataIn(imetadata0_in), .Write(write_tag_array), .BlockEnable(iblockenable), .DataOut(TagOut0_icache));
+  DataArray idatarray0(.clk(clk), .rst(rst), .DataIn(idata_in), .Write(write_data_array), .BlockEnable(iblockenable), .WordEnable(iwordenable), .DataOut(DataOut0_icache));
   // Way 1 icache
-  MetaDataArray imetarray1(.clk(clk), .rst(rst), .DataIn(imetadata1_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut1_icache));
-  DataArray idatarray1(.clk(clk), .rst(rst), .DataIn(idata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut1_icache));
+  MetaDataArray imetarray1(.clk(clk), .rst(rst), .DataIn(imetadata1_in), .Write(write_tag_array), .BlockEnable(iblockenable), .DataOut(TagOut1_icache));
+  DataArray idatarray1(.clk(clk), .rst(rst), .DataIn(idata_in), .Write(write_data_array), .BlockEnable(iblockenable), .WordEnable(iwordenable), .DataOut(DataOut1_icache));
 
   // data cache (TODO: Complete port hookups & check write enable doesnt need more)
   // Way 0 dcache
-  MetaDataArray dmetarray0(.clk(clk), .rst(rst), .DataIn(dmetadata0_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut0_dcache));
-  DataArray ddatarray0(.clk(clk), .rst(rst), .DataIn(ddata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut0_dcache));
+  MetaDataArray dmetarray0(.clk(clk), .rst(rst), .DataIn(dmetadata0_in), .Write(write_tag_array), .BlockEnable(dblockenable), .DataOut(TagOut0_dcache));
+  DataArray ddatarray0(.clk(clk), .rst(rst), .DataIn(ddata_in), .Write(write_data_array), .BlockEnable(dblockenable), .WordEnable(dwordenable), .DataOut(DataOut0_dcache));
   // Way 1 dcache
-  MetaDataArray dmetarray1(.clk(clk), .rst(rst), .DataIn(dmetadata1_in), .Write(write_tag_array), .BlockEnable(curr_pc[9:4]), .DataOut(TagOut1_dcache));
-  DataArray ddatarray1(.clk(clk), .rst(rst), .DataIn(ddata_in), .Write(write_data_array), .BlockEnable(curr_pc[9:4]), .WordEnable(memory_address[3:0]), .DataOut(DataOut1_dcache));
+  MetaDataArray dmetarray1(.clk(clk), .rst(rst), .DataIn(dmetadata1_in), .Write(write_tag_array), .BlockEnable(dblockenable), .DataOut(TagOut1_dcache));
+  DataArray ddatarray1(.clk(clk), .rst(rst), .DataIn(ddata_in), .Write(write_data_array), .BlockEnable(dblockenable), .WordEnable(dwordenable), .DataOut(DataOut1_dcache));
 
 
 
@@ -72,6 +98,8 @@ module CacheControl(
 
   
   // Determine cache miss (TODO: WHAT BITS TO CHECK FOR TAG)
+  wire[1:0] icache_hit_miss = ((curr_pc[15:10] === TagOut0_icache[5:0]) & TagOut0_icache[7]) ? 1'b00 : 
+
   assign icache_miss_detected = ((curr_pc[15:10] === TagOut0_icache[5:0]) & TagOut0_icache[7]) || ((curr_pc[15:10] === TagOut1_icache[5:0]) & TagOut1_icache[7]) ||
                                 ((curr_pc[15:10] === DataOut0_icache[5:0]) & TagOut0_icache[7])|| ((curr_pc[15:10] === DataOut1_icache[5:0]) & TagOut1_icache[7]);
 
@@ -79,6 +107,15 @@ module CacheControl(
                                 (curr_pc[15:10] === DataOut0_dcache[5:0]) & TagOut0_dcache[7]|| ((curr_pc[15:10] === DataOut1_dcache[5:0])& TagOut1_dcache[7]);
 
   
+  //Valid bit logic
+  // Valid bit is set to 0 when the process starts up it sets it 1 when there are misses in the cache.
+
+  assign dvalid0_bit = (dcache_miss_detected & ~dLRU0_bit) ? 1'b1 : dvalid0_bit;
+  assign dvalid1_bit = (dcache_miss_detected & ~dLRU1_bit) ? 1'b1 : dvalid1_bit;
+
+  assign ivalid0_bit = (icache_miss_detected & ~iLRU0_bit) ? 1'b1 : ivalid0_bit;
+  assign ivalid1_bit = (icache_miss_detected & ~iLRU1_bit) ? 1'b1 : ivalid1_bit;
+
   // input data to be written to dcache
   assign ddata_in = MainMemOut;
   assign dmetadata0_in = {dLRU0_bit, dvalid0_bit, memory_address[15:10]};
@@ -90,10 +127,43 @@ module CacheControl(
   assign imetadata1_in = {iLRU1_bit, ivalid1_bit, memory_address[15:10]};
 
  
-  // Memory module (TODO: WHAT IS DATA INPUT)
+  // Memory module (TODO: WHAT IS DATA INPUT) ///Contention
   memory4c mainmem(.data_out(MainMemOut), .data_in(), .addr(memory_address), .enable(icache_miss_detected | dcache_miss_detected), 
                    .wr(write_data_array), .clk(clk), .rst(rst), .data_valid(memory_data_valid));
 
 endmodule
 
 
+module decoder3_8 (
+  input [2:0] decode_in,
+  output [7:0] decode_out
+);
+  assign decode_out = (decode_in == 3'b000) ? 8'b00000001 :
+             (decode_in == 3'b001) ? 8'b00000010 :
+             (decode_in == 3'b010) ? 8'b00000100 :
+             (decode_in == 3'b011) ? 8'b00001000 :
+             (decode_in == 3'b100) ? 8'b00010000 :
+             (decode_in == 3'b101) ? 8'b00100000 :
+             (decode_in == 3'b110) ? 8'b01000000 :
+             8'b11111111;
+  
+endmodule
+
+
+module decoder6_64 (
+  input [5:0] decode_in,
+  output [63:0] decode_out,
+);
+
+  wire [63:0] b0, b1, b2, b3, b4, b5, b6;
+
+  assign b0 = 128'b1;
+  assign b1 = decode_in[0] ? (b0 << 1) : b0;
+  assign b2 = decode_in[1] ? (b1 << 2) : b1;
+  assign b3 = decode_in[2] ? (b2 << 4) : b2;
+  assign b4 = decode_in[3] ? (b3 << 8) : b3;
+  assign b5 = decode_in[4] ? (b4 << 16) : b4;
+  assign b6 = decode_in[5] ? (b5 << 32) : b5;
+  assign decode_out = b6;
+
+endmodule
--- cache.v
+++ cache.v
@@ -1,19 +1,103 @@
 module cache (
-  input clk, rst, WriteEnable0, WriteEnable1, MetaDataWriteEnable0, MetaDataWriteEnable1,
-  input [15:0] DataIn0, DataIn1,
-  input [7:0] MetaDataIn0, MetaDataIn1,
-  input [63:0] BlockEnable,
-  input [7:0] WordEnable,
-  output [15:0] Dataout0, Dataout1,
-  output [7:0] MetaDataOut0, MetaDataOut1
+  input clk, rst, cacheRead, cacheWrite,
+  input [15:0] address, data_in,
+  output miss,
+  output [15:0] data_out
 );
 
-//module DataArray(input clk, input rst, input [15:0] DataIn, input Write, input [63:0] BlockEnable, input [7:0] WordEnable, output [15:0] DataOut);
-DataArray dataarray0(.clk(clk), .rst(rst), .DataIn(DataIn0), .Write(WriteEnable0), .BlockEnable(BlockEnable), .WordEnable(WordEnable), .DataOut(Dataout0));
-DataArray dataarray1(.clk(clk), .rst(rst), .DataIn(DataIn1), .Write(WriteEnable1), .BlockEnable(BlockEnable), .WordEnable(WordEnable), .DataOut(Dataout1));
+  //Wires for holding the address bits
+  wire [5:0] tag_bits, set_bits;
+  wire [3:0] block_offset_bits;
 
-//module MetaDataArray(input clk, input rst, input [7:0] DataIn, input Write, input [63:0] BlockEnable, output [7:0] DataOut);
-MetaDataArray metadataarray0(.clk(clk), .rst(rst), .DataIn(MetaDataIn0), .Write(MetaDataWriteEnable0), .BlockEnable(BlockEnable), .DataOut(MetaDataOut0));
-MetaDataArray metadataarray0(.clk(clk), .rst(rst), .DataIn(MetaDataIn1), .Write(MetaDataWriteEnable1), .BlockEnable(BlockEnable), .DataOut(MetaDataOut1));
+  //Wires for block and word
+  wire [63:0] blockenable;
+  wire [7:0] wordenable;
+
+  //Wires for the meta data array
+  wire [7:0] metaout0, metaout1; //This is the one used for comparing
+  wire [7:0] metain0, metain1;
+  
+  //Keeps track of the hit and miss stuff
+  wire way0hit, way1hit, miss;
+
+  //Cache output
+  wire [15:0] data_out0, data_out1;
+
+  //split the bits
+  assign tag_bits = address[15:10];
+  assign set_index = address[9:4];
+  assign block_offset_bits = address[3:1];
+
+  //decode the set/block
+  decoder6_64 blockdecoder(.decode_in(set_bits), .decode_out(blockenable));
+  //decode the word
+  decoder3_8 worddecoder(.decode_in(block_offset_bits), .decode_out(wordenable)); 
+
+  //check for a hit or a miss
+  assign way0hit = ((tag_bits === metaout0[5:0]) & metaout0[6]) ? 1'b1 : 1'b0;
+  assign way1hit = ((tag_bits === metaout1[5:0]) & metaout1[6]) ? 1'b1 : 1'b0;
+  assign miss = way0hit | way1hit;
+
+  //if there is a miss and we need to evict or no data in cache assign to way0.
+  assign evict = ((metaout0[7] === 1'b1) & miss) ? 1'b1 : ((metaout1[7] === 1'b1) & miss) ? 1'b0 : 1'b0; 
+
+  //calculate the new meata data in.
+  assign metain0 = (way0hit) ? {1'b1, 1'b1, tag_bits} : 
+                   (way1hit) ? {1'b0, metaout0[6], metaout0[5:0]} :
+                   (miss & ~evict) ? {1'b1, 1'b1, tag_bits} : 
+                   7'bz;  //condition should not hit
+                
+  assign metain1 = (way1hit) ? {1'b1, 1'b1, tag_bits} : 
+                   (way0hit) ? {1'b0, metaout1[6], metaout1[5:0]} :
+                   (miss & evict) ? {1'b1, 1'b1, tag_bits} : 
+                   7'bz; //condition should not hit
+
+  
+
+  // Way 0 icache
+  MetaDataArray metarray0(.clk(clk), .rst(rst), .DataIn(metain0), .Write(cacheRead | cacheWrite), .BlockEnable(blockenable), .DataOut(metaout0));
+  DataArray datarray0(.clk(clk), .rst(rst), .DataIn(data_in), .Write(cacheWrite), .BlockEnable(blockenable), .WordEnable(wordenable), .DataOut(data_out0));
+
+  // Way 1 icache
+  MetaDataArray metarray1(.clk(clk), .rst(rst), .DataIn(metain1), .Write(cacheRead | cacheWrite), .BlockEnable(blockenable), .DataOut(metaout1));
+  DataArray datarray1(.clk(clk), .rst(rst), .DataIn(data_in), .Write(cacheWrite), .BlockEnable(blockenable), .WordEnable(wordenable), .DataOut(data_out1));
+
+
+  assign data_out = (way0hit) ? data_out0 : data_out1;
+
+endmodule
+
+
+module decoder3_8 (
+  input [2:0] decode_in,
+  output [7:0] decode_out
+);
+  assign decode_out = (decode_in == 3'b000) ? 8'b00000001 :
+             (decode_in == 3'b001) ? 8'b00000010 :
+             (decode_in == 3'b010) ? 8'b00000100 :
+             (decode_in == 3'b011) ? 8'b00001000 :
+             (decode_in == 3'b100) ? 8'b00010000 :
+             (decode_in == 3'b101) ? 8'b00100000 :
+             (decode_in == 3'b110) ? 8'b01000000 :
+             8'b11111111;
+  
+endmodule
+
+
+module decoder6_64 (
+  input [5:0] decode_in,
+  output [63:0] decode_out,
+);
+
+  wire [63:0] b0, b1, b2, b3, b4, b5, b6;
+
+  assign b0 = 128'b1;
+  assign b1 = decode_in[0] ? (b0 << 1) : b0;
+  assign b2 = decode_in[1] ? (b1 << 2) : b1;
+  assign b3 = decode_in[2] ? (b2 << 4) : b2;
+  assign b4 = decode_in[3] ? (b3 << 8) : b3;
+  assign b5 = decode_in[4] ? (b4 << 16) : b4;
+  assign b6 = decode_in[5] ? (b5 << 32) : b5;
+  assign decode_out = b6;
 
 endmodule
